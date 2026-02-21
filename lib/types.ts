e59f8a90a5ab895dc0ae923728cb1d9b633f9614
Ephy Kimotho <ephyk48@gmail.com>
2026-02-21 20:28:51 +0300

    chore: added invitation types

--- lib/types.ts
+++ lib/types.ts
@@ -1,3 +1,16 @@
 // This file has all application interfaces and types
 
-export type Role = "user" | "admin" | "auditor" | "inventoryManager"
+export type Role = "user" | "admin" | "auditor" | "inventory_manager"
+
+export interface InviteUserInput {
+    name: string;
+    role: Role;
+    email: string;
+    invitedById: string;
+}
+
+export interface AcceptInvitationInput {
+    token: string,
+    name: string,
+    password: string
+}
