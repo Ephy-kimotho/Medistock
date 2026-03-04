741f2cd78b052ee21e5bf0a6c63745a371eec129
Ephy Kimotho <ephyk48@gmail.com>
2026-03-04 23:33:22 +0300

    chore: exported ban and unban functions

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,7 +2,7 @@ import { createAuthClient } from 'better-auth/react'
 import { adminClient } from "better-auth/client/plugins"
 import { ac, admin, auditor, inventory_manager, user } from "@/lib/permissions"
 
-export const { signIn, signOut, useSession, requestPasswordReset, resetPassword } = createAuthClient({
+export const { signIn, signOut, useSession, requestPasswordReset, resetPassword, admin: { banUser, unbanUser } } = createAuthClient({
     plugins: [
         adminClient({
             ac,
